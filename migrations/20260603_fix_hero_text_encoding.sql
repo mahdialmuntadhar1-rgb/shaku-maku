a989ea606ed699ffd04ca3512ce0843bf451a3ec
Mahdi <mahdialmuntadhar1@gmail.com>
2026-06-03 20:44:38 +0300

    fix phase 4A: deploy hero API to main worker and repair hero text encoding

---
+++ migrations/20260603_fix_hero_text_encoding.sql
@@ -0,0 +1,29 @@
+UPDATE hero_slides
+SET
+  slogan_ar = 'اكتشف أفضل الأماكن حولك',
+  slogan_ku = 'باشترین شوێنەکان لە دەوروبەرت بدۆزەرەوە',
+  slogan_en = 'Discover the best places around you',
+  badge_ar = 'مطاعم وكافيهات',
+  badge_ku = 'چێشتخانە و کافێ',
+  badge_en = 'Restaurants & Cafes'
+WHERE id = 'hero-default-iraq-food';
+
+UPDATE hero_slides
+SET
+  slogan_ar = 'كل الأعمال العراقية في مكان واحد',
+  slogan_ku = 'هەموو کارە عێراقییەکان لە یەک شوێن',
+  slogan_en = 'Iraqi businesses in one place',
+  badge_ar = 'دليل الأعمال',
+  badge_ku = 'ڕێبەری کار',
+  badge_en = 'Business Hub'
+WHERE id = 'hero-default-local-business';
+
+UPDATE hero_slides
+SET
+  slogan_ar = 'تابع العروض والمنشورات الجديدة',
+  slogan_ku = 'داشکاندن و پۆستە نوێکان ببینە',
+  slogan_en = 'Follow offers and new posts',
+  badge_ar = 'نبض الشارع',
+  badge_ku = 'پۆستە نوێکان',
+  badge_en = 'Social Feed'
+WHERE id = 'hero-default-social-feed';
